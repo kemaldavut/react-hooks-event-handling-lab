81977e53e82d9b11c24ad54b2d860dcc6244c7af
Kemal DAVUT <mustafakemaldavut@gmail.com>
2022-08-01 16:18:42 +0300

    Completed assignment

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,16 @@
-// Code Keypad Component Here
-
-function Keypad (){
+function Keypad() {
+    function handleChange(event) {
+      console.log("Entering password...");
+    }
     return (
-        <div></div>
-    )
-}
-
-export default Keypad;
+      <div>
+        <input
+          type="password"
+          onChange={handleChange}
+          placeholder="Enter password..."
+        ></input>
+      </div>
+    );
+  }
+  
+  export default Keypad;
